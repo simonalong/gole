05dfe8477008124f539ac8fc171b30f42ab9c931
shizi <lan.zhu.123@163.com>
2022-02-17 17:44:56 +0800

    update 更新

--- config/configHelper.go
+++ config/configHelper.go
@@ -18,7 +18,7 @@ var appProperty *ApplicationProperty
 // 支持yml、yaml、json、properties格式
 // 优先级yaml > yml > properties > json
 func LoadConfig() {
-	LoadConfigWithRelativePath("./resources/")
+	LoadConfigWithRelativePath("")
 }
 
 // LoadConfigWithRelativePath 加载相对文件路径，相对路径是相对系统启动的位置部分
